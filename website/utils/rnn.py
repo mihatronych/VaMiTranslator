fe96b4922cf58753b68225293b5c4d823adbf3c6
mihaj <dolgushin.mikhail131200@yandex.ru>
2024-01-14 15:51:46 +0300

    Интеграция RNN работает.

--- website/utils/rnn.py
+++ website/utils/rnn.py
@@ -1,10 +1,10 @@
 import numpy as np
-import utils
-from constants import target_tv, source_tv
+from . import utilities
+from . import constants
 
-target_vectorization = utils.read_pickle_tv(target_tv)
-source_vectorization = utils.read_pickle_tv(source_tv)
-
+target_vectorization = utilities.read_pickle_tv(constants.target_tv_path)
+source_vectorization = utilities.read_pickle_tv(constants.source_tv_path)
+seq2seq_rnn = utilities.reconstruct_model_from_h5(constants.seq2seq_rnn_path)
 spa_vocab = target_vectorization.get_vocabulary()
 spa_index_lookup = dict(zip(range(len(spa_vocab)), spa_vocab))
 max_decoded_sentence_length = 20
@@ -12,7 +12,7 @@ max_decoded_sentence_length = 20
 
 def decode_sequence_rnn(input_sentence):
     tokenized_input_sentence = source_vectorization([input_sentence])
-    decoded_sentence = "[start]"
+    decoded_sentence = "start"
     for i in range(max_decoded_sentence_length):
         tokenized_target_sentence = target_vectorization([decoded_sentence])
         next_token_predictions = seq2seq_rnn.predict(
@@ -20,6 +20,7 @@ def decode_sequence_rnn(input_sentence):
         sampled_token_index = np.argmax(next_token_predictions[0, i, :])
         sampled_token = spa_index_lookup[sampled_token_index]
         decoded_sentence += " " + sampled_token
-        if sampled_token == "[end]":
+        if sampled_token == "end":
             break
-    return decoded_sentence
+
+    return " ".join(decoded_sentence.split()[1:-1])
